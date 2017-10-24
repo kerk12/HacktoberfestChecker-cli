3335198bffa11b38fa9bec903e87184b27de5d6e
Reuben MOSWELA <rmoswela@e4r6p11.wethinkcode.co.za>
2017-10-24 08:48:59 +0200

    corrected grammar, added indentation and replace multiline comments

--- hfc.py
+++ hfc.py
@@ -1,9 +1,8 @@
 import requests
 
 class PullRequest:
-	'''
-	Constructor and initialization of instance variables
-	'''
+	
+	# Constructor and initialization of instance variables
     def __init__(self, title, url, repo, number, created_at):
         self.title = title
         self.url = url
@@ -11,17 +10,13 @@ class PullRequest:
         self.created_at = created_at  # TODO Might do something with it later
         self.number = number
 
-'''
-overloading the string representation of the object
-'''
+	# Overloading the string representation of the object
     def __str__(self):
         output = ""
         output += '{} (#{}) -> {}'.format(self.title, self.number,self.url)
         return output
-'''
-check if the file is called as an executable
-and call the functions within
-'''
+
+# Check if the file is called as an executable
 if __name__ == "__main__":
     print("""Hacktoberfest Checker CLI
 Developed by Kyriakos Giannakis
@@ -31,25 +26,22 @@ Licenced under GNU/GPLv3
     
 """)
 
-#variable to hold the github username
+	# Github Username
     gh_name = input("Please enter your github username: ")
 
-#variable req to hold data about the issues created/opened
+	# Request object holding PR data from the github API
     req = requests.get("https://api.github.com/search/issues?q=author:{}%20type:pr%20created:%3E2017-09-30%20created:%3C2017-11-01".format(gh_name))
 
-    json_out = req.json() #converts values in req variable to json representation
-	'''
-	check for errors in variable json_out
-	'''
+    json_out = req.json()  # Converts values in req variable to json representation
+	
+	# Check for errors in variable json_out
     if "errors" in json_out:
         print("The username you typed does not exist or you don't have access to view this user's profile.")
         exit(1)
-    prs = [] #create a new list
-    pr_count = json_out["total_count"] #get the value stored in key total count
+    prs = []
+    pr_count = json_out["total_count"]  # Get the number of PRs submitted by the user
 
-'''
-check the number of pull requests and store them as an object
-'''
+	# Checks the number of pull requests and stores them as objects
     if pr_count > 0:
         for item in json_out["items"]:
             pr = PullRequest(title=item["title"],
@@ -57,20 +49,18 @@ check the number of pull requests and store them as an object
                              number=item["number"],
                              repo=item["repository_url"],
                              created_at=item["created_at"])
-            prs.append(pr) #adds object that holds pull request data to end of list
+            prs.append(pr)  # Adds object that holds pull request data to end of list
     else:
         print("0/0\nNo pull requests yet.")
         exit(0)
 
     print("You have completed {}/4 pull requests:\n".format(pr_count))
-'''
-prints out whats stored in the list
-'''
+	
+	# Prints out whats stored in the list
     for pr in prs:
         print(pr)
-'''
-checks if the number of pull request are enough to have completed the challenge
-'''
+
+	# Checks if the number of pull request are enough to have completed the challenge
     if pr_count >= 4:
         print("\nCongratulations! You have completed the Hacktoberfest challenge!")
 
