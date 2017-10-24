3d1a749f0a0665dc736733442d3e5f058788fe94
Lucas <lucasinfologos@gmail.com>
2017-10-24 20:51:52 -0300

    Added basic exception handling to check internet connection

--- hfc.py
+++ hfc.py
@@ -32,33 +32,36 @@ Licenced under GNU/GPLv3
     gh_name = ""
     while gh_name == "":
         gh_name = input("Please enter your github username: ")
-    #Make request to github api
-    req = requests.get("https://api.github.com/search/issues?q=author:{}%20type:pr%20created:%3E2017-09-30%20created:%3C2017-11-01".format(gh_name))
-    #Get a json from the request
-    json_out = req.json()
-    if "errors" in json_out:
-        print("The username you typed does not exist or you don't have access to view this user's profile.")
-        exit(1)
-    prs = []
-    pr_count = json_out["total_count"]
+    try:
+        #Make request to github api
+        req = requests.get("https://api.github.com/search/issues?q=author:{}%20type:pr%20created:%3E2017-09-30%20created:%3C2017-11-01".format(gh_name))
+        #Get a json from the request
+        json_out = req.json()
+        if "errors" in json_out:
+            print("The username you typed does not exist or you don't have access to view this user's profile.")
+            exit(1)
+        prs = []
+        pr_count = json_out["total_count"]
 
-    if pr_count > 0:
-        for item in json_out["items"]:
-            pr = PullRequest(title=item["title"],
-                             url=item["url"],
-                             number=item["number"],
-                             repo=item["repository_url"],
-                             created_at=item["created_at"])
-            prs.append(pr)
-    else:
-        print("0/0\nNo pull requests yet.")
-        exit(0)
+        if pr_count > 0:
+            for item in json_out["items"]:
+                pr = PullRequest(title=item["title"],
+                                url=item["url"],
+                                number=item["number"],
+                                repo=item["repository_url"],
+                                created_at=item["created_at"])
+                prs.append(pr)
+        else:
+            print("0/0\nNo pull requests yet.")
+            exit(0)
 
-    print("You have completed {}/4 pull requests:\n".format(pr_count))
-    for pr in prs:
-        print(pr)
-    #If the number of pull request that you have is equals or less than 4 the profile don't have completed the challenge
-    if pr_count >= 4:
-        print("\nCongratulations! You have completed the Hacktoberfest challenge!")
+        print("You have completed {}/4 pull requests:\n".format(pr_count))
+        for pr in prs:
+            print(pr)
+        #If the number of pull request that you have is equals or less than 4 the profile don't have completed the challenge
+        if pr_count >= 4:
+            print("\nCongratulations! You have completed the Hacktoberfest challenge!")
+    except requests.RequestException:
+        print('\nERROR CONNECTING TO THE INTERNET\n')
 
     exit(0)
